4c6b7cc355a246c754fbeeec8e80f40cd69b81e8
aruizc <ing.angelruizcerrillo@gmail.com>
2024-12-06 20:42:02 -0600

    Additional unit test for L337 for no captured units

--- test/server/cards/02_SHD/units/L3-37DroidRevolutionary.spec.ts
+++ test/server/cards/02_SHD/units/L3-37DroidRevolutionary.spec.ts
@@ -1,7 +1,7 @@
 describe('L3-37, Droid Revolutionary ability', function() {
     integration(function(contextRef) {
         describe('L3-37, Droid Revolutionary\'s when played ability', function() {
-            beforeEach(function () {
+            it('rescue a unit and no shield given', function() {
                 contextRef.setupTest({
                     phase: 'action',
                     player1: {
@@ -20,10 +20,6 @@ describe('L3-37, Droid Revolutionary ability', function() {
                 context.player1.clickCard(context.takeCaptive);
 
                 expect(context.wingLeader).toBeCapturedBy(context.tielnFighter);
-            });
-
-            it('rescue a unit and no shield given', function() {
-                const { context } = contextRef;
 
                 // P2 Rescues Wing Leader with L3-37
                 context.player2.clickCard(context.l337DroidRevolutionary);
@@ -35,7 +31,24 @@ describe('L3-37, Droid Revolutionary ability', function() {
             });
 
             it('does not rescue a unit and is shielded', function() {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['take-captive'],
+                        spaceArena: ['tieln-fighter']
+                    },
+                    player2: {
+                        spaceArena: ['wing-leader'],
+                        hand: ['l337#droid-revolutionary']
+                    }
+                });
+
                 const { context } = contextRef;
+
+                // SETUP: P1 Captures Wing Leader with Tie/LN
+                context.player1.clickCard(context.takeCaptive);
+
+                expect(context.wingLeader).toBeCapturedBy(context.tielnFighter);
 
                 context.player2.clickCard(context.l337DroidRevolutionary);
                 expect(context.player2).toHavePassAbilityPrompt('Rescue a captured card');
@@ -44,6 +57,25 @@ describe('L3-37, Droid Revolutionary ability', function() {
 
                 expect(context.wingLeader).toBeCapturedBy(context.tielnFighter);
                 expect(context.player1).toBeActivePlayer();
+                expect(context.l337DroidRevolutionary).toHaveExactUpgradeNames(['shield']);
+            });
+
+            it('does not rescue a unit as there is no captured units and is shielded', function() {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['l337#droid-revolutionary']
+                    },
+                    player2: {
+                        groundArena: ['atst']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.l337DroidRevolutionary);
+
+                expect(context.player2).toBeActivePlayer();
                 expect(context.l337DroidRevolutionary).toHaveExactUpgradeNames(['shield']);
             });
         });
