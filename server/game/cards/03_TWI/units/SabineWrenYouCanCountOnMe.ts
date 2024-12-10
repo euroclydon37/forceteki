d5e4ccadd6176a0544cf33288ae33342d8cc5655
Valentin Jubert <val_jub@live.fr>
2024-12-10 13:36:50 +0100

    sabine wren

---
+++ server/game/cards/03_TWI/units/SabineWrenYouCanCountOnMe.ts
@@ -0,0 +1,43 @@
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import AbilityHelper from '../../../AbilityHelper';
+import { AbilityRestriction, Aspect, RelativePlayer, WildcardCardType, ZoneName } from '../../../core/Constants';
+
+export default class SabineWrenYouCanCountOnMe extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '2843644198',
+            internalName: 'sabine-wren#you-can-count-on-me',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addConstantAbility({
+            title: 'While this unit is exhausted, she can\'t be attacked',
+            condition: (context) => context.source.exhausted,
+            ongoingEffect: AbilityHelper.ongoingEffects.cardCannot(AbilityRestriction.BeAttacked)
+        });
+
+        this.addOnAttackAbility({
+            title: 'Discard the top card from your deck',
+            optional: true,
+            immediateEffect: AbilityHelper.immediateEffects.discardSpecificCard((context) => ({
+                target: context.source.controller.getTopCardOfDeck()
+            })),
+            ifYouDo: (ifYouDoContext) => ({
+                title: 'If it doesn\'t share an aspect with your base, deal 2 damage to a ground unit',
+                immediateEffect: AbilityHelper.immediateEffects.conditional({
+                    condition: (context) => !ifYouDoContext.events[0].card.aspects.includes(context.source.controller.base.aspects[0]),
+                    onTrue: AbilityHelper.immediateEffects.selectCard({
+                        cardTypeFilter: WildcardCardType.Unit,
+                        controller: RelativePlayer.Opponent,
+                        zoneFilter: ZoneName.GroundArena,
+                        innerSystem: AbilityHelper.immediateEffects.damage({ amount: 2 })
+                    }),
+                    onFalse: AbilityHelper.immediateEffects.noAction(),
+                })
+            })
+        });
+    }
+}
+
+SabineWrenYouCanCountOnMe.implemented = true;
