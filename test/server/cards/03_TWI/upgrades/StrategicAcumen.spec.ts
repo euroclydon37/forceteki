22df0c2b14cdd99329eec55a0681cdee9c93f17e
Jeremy <jeremydunn29@gmail.com>
2024-12-13 12:14:01 -0600

    start on the test

--- test/server/cards/03_TWI/upgrades/StrategicAcumen.spec.ts
+++ test/server/cards/03_TWI/upgrades/StrategicAcumen.spec.ts
@@ -1,7 +1,29 @@
 describe('Strategic Acumen', function () {
     integration((contextRef) => {
-        it('is not yet implemented', () => {
-            expect(true).toBe(false);
+        describe('Strategic Acumen\'s ability', () => {
+            beforeEach(() => {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['strategic-acumen', 'alliance-xwing'],
+                        groundArena: ['battlefield-marine'],
+                    },
+                });
+            });
+
+            it('should let the player play a unit for 1 less resources', () => {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.strategicAcumen);
+                context.player1.clickCard(context.battlefieldMarine);
+
+                context.player2.passAction();
+
+
+                // alliance-xwing should be played for 1 less resources
+                // expect(context.player1.resources).toBe(2);
+                // expect(context.battlefieldMarine).toBeInPlay();
+            });
         });
     });
 });
